7c780306f7c1c436d6987073d039599de24f36d6
Matt Elliott <mattdelliott@me.com>
2014-11-04 15:10:46 -0700

    Added in Grunt task for minified version with map.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,25 @@
 module.exports = function (grunt) {
 
-    
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        uglify: {
+            js: {
+                options: {
+                    sourceMap: true,
+                    sourceMapName: 'flipper.min.js.map'
+                },
+                files: {
+                    'flipper.min.js': ['src/flipper.js']
+                }
+            }
+        }
+    });
+
+    grunt.registerTask('default',[
+        'copy:js',
+        'uglify:js'
+    ]);
 
 };
